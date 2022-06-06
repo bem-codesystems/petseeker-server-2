fcb5e517a7e25d274ea4b201a2144227519ade9d
adrianohellermylla <adrianohellermylla@gmail.com>
2022-06-06 00:54:49 -0300

    feat: Add User struct

---
+++ src/user.rs
@@ -0,0 +1,37 @@
+use chrono::{DateTime, Utc};
+
+pub struct User {
+    id: String,
+    name: String,
+    email: String,
+    created_at: DateTime<Utc>,
+    updated_at: DateTime<Utc>,
+    has_wallet: bool,
+    wallet_id: String,
+}
+
+impl User {
+    pub fn new(id: String,name: String,email: String, has_wallet: bool, wallet_id: String) -> User {
+        User {
+            id,
+            name,
+            email,
+            created_at: Utc::now(),
+            updated_at: Utc::now(),
+            has_wallet,
+            wallet_id,
+        }
+    }
+    pub fn get(&self) -> User {
+        User {
+            id: String::from(&self.id),
+            name: String::from(&self.name),
+            email: String::from(&self.email),
+            created_at: Utc::now(),
+            updated_at: Utc::now(),
+            has_wallet: self.has_wallet,
+            wallet_id: String::from(&self.wallet_id),
+        }
+    }
+}
+
