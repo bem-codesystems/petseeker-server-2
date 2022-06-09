911f3c205d42ea8cda7d103f8124438bb40d7105
adrianohellermylla <adrianohellermylla@gmail.com>
2022-06-09 00:04:25 -0300

    feat: Add Drone struct

--- src/drone.rs
+++ src/drone.rs
@@ -1,11 +1,26 @@
+#[derive(Debug)]
 pub enum CamType {
     Normal,
     Thermal
 }
-
+#[derive(Debug)]
+pub enum DroneSource {
+    Own,
+    ThirdPartyProvider,
+}
+#[derive(Debug)]
+pub enum DroneBatteryState {
+    Low,
+    Medium,
+    SemiFull,
+    Full,
+}
 #[derive(Debug)]
 pub struct Drone<'d> {
     id: String,
     has_cam: bool,
-    cam_type: &'d CamType
+    cam_type: &'d CamType,
+    source: &'d DroneSource,
+    battery_state: &'d DroneBatteryState,
+    charge_level: u8,
 }
