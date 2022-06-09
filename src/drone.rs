05d2ae227fa7760e41f5dec77f73f927b445c8ae
adrianohellermylla <adrianohellermylla@gmail.com>
2022-06-09 00:21:13 -0300

    feat: Add Drone struct

--- src/drone.rs
+++ src/drone.rs
@@ -30,3 +30,35 @@ pub struct Drone<'d> {
     created_at: DateTime<Utc>,
     updated_at: DateTime<Utc>,
 }
+
+impl<'d> Drone<'d> {
+    pub fn new (id: String, has_cam: bool, cam_type: &'d CamType, source: &'d DroneSource,
+               battery_state: &'d DroneBatteryState, charge_level: u8, manufacturer: String, model: String) -> Drone<'d> {
+        Drone {
+            id,
+            has_cam,
+            cam_type,
+            source,
+            battery_state,
+            charge_level,
+            manufacturer,
+            model,
+            created_at: Utc::now(),
+            updated_at: Utc::now(),
+        }
+    }
+    pub fn get (&self) -> Drone<'d> {
+        Drone {
+            id: String::from(&self.id),
+            has_cam: self.has_cam,
+            cam_type: self.cam_type,
+            source: self.source,
+            battery_state: self.battery_state,
+            charge_level: self.charge_level,
+            manufacturer: String::from(&self.manufacturer),
+            model: String::from(&self.model),
+            created_at: Utc::now(),
+            updated_at: Utc::now(),
+        }
+    }
+}
