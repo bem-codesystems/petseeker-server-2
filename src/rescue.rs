a9038b891fa2bed8423dced2987d4d14ced3865e
adrianohellermylla <adrianohellermylla@gmail.com>
2022-06-11 00:18:15 -0300

    feat: Create rescue struct

--- src/rescue.rs
+++ src/rescue.rs
@@ -29,5 +29,30 @@ pub struct Rescue<'l> {
     has_success: bool,
     risk: &'l RescueRiskLevel,
     need_hospital: bool,
-    rescue_wallet: Vec<&'l Wallet<'l>>
+    rescue_wallet: Vec<&'l Wallet<'l>>,
+    inform_police: bool,
+}
+
+impl<'l> Rescue<'l> {
+    pub fn new(id: String,pet: &'l Pet, user: &'l User, vet: &'l Vet, address: String, complement: String, number: String,
+               place_type: &'l RescuePlaceType, rescue_lat: String, rescue_lng: String, has_success: bool, risk: &'l RescueRiskLevel,
+               need_hospital: bool, rescue_wallet: Vec<&'l Wallet>,inform_police: bool) -> Rescue<'l> {
+        Rescue {
+            id,
+            pet,
+            user,
+            vet,
+            address,
+            complement,
+            number,
+            place_type,
+            rescue_lat,
+            rescue_lng,
+            has_success,
+            risk,
+            need_hospital,
+            rescue_wallet,
+            inform_police
+        }
+    }
 }
